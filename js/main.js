e84db72cd3bb8c6ae2c8a532c9e9bdd03eaadc30
Modas <modestas.mar@gmail.com>
2019-11-05 10:43:37 +0200

    Kintamieji

--- js/main.js
+++ js/main.js
@@ -15,3 +15,64 @@ console.log( ['a', 87, 'b', 5451, 'c', [1, 2, 's']] );
 
 // objektai - object
 console.log( {name: 'Rimantas', age: 31.8} );
+
+console.log('-------------------------');
+console.log('-------------------------');
+console.log('-------------------------');
+
+const PI = 3.141592654;
+console.log(PI);
+
+const vardas = 'Rimantas';
+console.log(vardas);
+
+let manoAmzius = 9;
+console.log(manoAmzius);
+
+manoAmzius = 10;
+console.log(manoAmzius);
+
+const sunsVardas = 'Rexas';
+console.log(sunsVardas);
+
+let miestas = "Vilnius";
+console.log(miestas);
+
+let personazas = "Don' Ki Chotas";
+console.log(personazas);
+
+const kabutes = 'Kabuciu buna: viengubos(\') ir dvygubos(").';
+console.log(kabutes);
+
+const kabutes2 = "Kabuciu buna: viengubos(') ir dvygubos(\").";
+console.log(kabutes2);
+
+var kintamasis = 'x';
+console.log(kintamasis);
+
+const skaitmenys = [9, 1, 7];
+console.log(skaitmenys);
+
+const raides = ['', 'a', 'b', 'c'];
+console.log(raides);
+
+let arBlaivas = true;
+console.log(arBlaivas);
+
+console.log('-------------------------');
+console.log('-------------------------');
+console.log('-------------------------');
+
+const suma = 2 + 2;
+console.log(suma);
+
+const suma2 = manoAmzius + 7;
+console.log(suma2);
+
+const tekstas = 'abc' + 'efg';  // aa
+console.log(tekstas);
+
+console.log(skaitmenys);
+
+const skaitmenuSuma = skaitmenys[0] + skaitmenys[1] + skaitmenys[2];
+console.log(skaitmenuSuma);
