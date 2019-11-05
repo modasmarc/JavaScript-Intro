e72c4d3a982249d95cc84b91a5d7996c8dc67451
Modas <modestas.mar@gmail.com>
2019-11-05 10:41:03 +0200

    Kintamuju tipai

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// tekstai
+console.log('asfsgdghb');
+
+// skaiciai
+console.log(852545);
+
+// boolean - logine reiksme
+console.log(true);
+console.log(false);
+
+// sarasai - array
+console.log( [1, 2, 3, 4, 5] );
+console.log( ['a', 'b', 'c'] );
+console.log( ['a', 87, 'b', 5451, 'c', [1, 2, 's']] );
+
+// objektai - object
+console.log( {name: 'Rimantas', age: 31.8} );
