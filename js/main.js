af2b62ea2e3a0dfb382c1cb0b6d241d3f19d159c
Modas <modestas.mar@gmail.com>
2019-11-06 09:41:41 +0200

    Palyginimas

--- js/main.js
+++ js/main.js
@@ -63,16 +63,154 @@ console.log('-------------------------');
 console.log('-------------------------');
 console.log('-------------------------');
 
-const suma = 2 + 2;
-console.log(suma);
-
-const suma2 = manoAmzius + 7;
-console.log(suma2);
-
-const tekstas = 'abc' + 'efg';  // aa
-console.log(tekstas);
-
 console.log(skaitmenys);
 
 const skaitmenuSuma = skaitmenys[0] + skaitmenys[1] + skaitmenys[2];
 console.log(skaitmenuSuma);
+
+const s1 = 2;
+const s2 = 15;
+
+const suma = s1 + s2;
+console.log(suma);
+
+const skirtumas = s1 - s2;
+console.log(skirtumas);
+
+const dalmuo = s1 / s2;
+console.log(dalmuo);
+
+const sandauga = s1 * s2;
+console.log(sandauga);
+
+const vidurkis = (s1 + s2) / 2;
+console.log(vidurkis);
+
+const pirmoSaknis = Math.sqrt( s1 );
+console.log(pirmoSaknis);
+
+const maziausias = Math.min(s1, s2);
+console.log(maziausias);
+
+const didziausiais = Math.max(s1, s2);
+console.log(didziausiais);
+
+const didziausiaisSarase = Math.max(skaitmenys);
+console.log(didziausiaisSarase);
+
+const t1 = 'Labas';
+const t2 = 'rytas';
+
+const tekstas = t1 + t2;
+console.log(tekstas);
+
+const pasisveikinimas = t1 + ' ' + t2;
+console.log(pasisveikinimas);
+
+// negalimos operacijos su ne skaiciais
+console.log( t1 - t2 );
+console.log( t1 * t2 );
+console.log( t1 / t2 );
+
+const abc = ['a', 'b', 'c', 'd', 'e'];
+console.log(abc);
+
+const abcIsGalo = abc[4] + ', ' + abc[3]
+                        + ', ' + abc[2]
+                        + ', ' + abc[1]
+                        + ', ' + abc[0];
+console.log(abcIsGalo);
+
+const taiKaipTen = true + false + true + 2;
+console.log(taiKaipTen);
+
+console.log('-------------------------');
+console.log('-------------------------');
+console.log('-------------------------');
+console.log('PALYGINIMAS');
+
+// >, <, ===, >=, <=, !==
+// s1 = 2; s2 = 15;
+
+const s2t = '15';
+
+if ( s2t !== s2 ) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandykite dar kartą');
+}
+
+const m = 0;
+
+if ( m < 0 ) {
+    console.log('Neigiamas');
+} else {
+    if ( m === 0 ) {
+        console.log('Nei neigiamas, nei teigiamas');
+    } else {
+        console.log('Teigiamas');
+    }
+}
+
+if ( m < 0 ) {
+    console.log('Neigiamas');
+}
+if ( m === 0 ) {
+    console.log('Nei neigiamas, nei teigiamas');
+}
+if ( m >0 ) {
+    console.log('Teigiamas');
+}
+
+if ( m < 0 ) {
+    console.log('Neigiamas');
+} else if ( m === 0 ) {
+    console.log('Nei neigiamas, nei teigiamas');
+} else {
+    console.log('Teigiamas');
+}
+
+const akys = 'melynas';
+
+if ( akys === 'rudos' ) {
+    console.log('Melagis');
+} else if ( akys === 'zalios' ) {
+    console.log('5%');
+} else if ( akys === 'melynas') {
+    console.log('Cia mano');
+} else {
+    console.log('A tu bent turi akis?');
+}
+
+
+if ( 4 > 2) {
+    console.log(true);
+}
+
+if ( '4' > 2) {
+    console.log(true);
+}
+
+if ( 4 > '2') {
+    console.log(true);
+}
+
+if ( '2' > '4') {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if ( '+' > 'Z') {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+const manoNumeris = '370612345678';
+
+if ( '370600000000' < manoNumeris) {
+    console.log(true);
+} else {
+    console.log(false);
+} 
